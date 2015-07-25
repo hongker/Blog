cf6c9ab17d5c45b40979b5d7a34e9881cb868669
hongker <xiaok2013@live.com>
2015-07-25 11:21:54 +0800

    add register function

--- apps/frontend/controllers/RegisterController.php
+++ apps/frontend/controllers/RegisterController.php
@@ -1,5 +1,6 @@
 <?php
 namespace Blog\Frontend\Controllers;
+use Blog\Operations\UserOperation;
 /**
  * 注册控制器
  * @author hongker
@@ -7,15 +8,40 @@ namespace Blog\Frontend\Controllers;
  */
 class RegisterController extends BaseController
 {
+	protected $operation;
 	public function initialize()
 	{
 		\Phalcon\Tag::setTitle('注册');
 		parent::initialize();
 		$this->view->setTemplateAfter('common');
+		
+		$this->operation = new UserOperation($this->di);
 	}
 
 	public function indexAction() {
 		
+	}
+	
+	/**
+	 * 添加用户
+	 */
+	public function addUser() {
+		$return = array();
+		if($this->request->isPost()) {
+			$data['username'] = $this->request->getPost('username','string');
+			$data['email'] = $this->request->getPost('email','email');
+			$data['password'] = $this->security->hash($this->request->getPost('password','string'));
+			
+			if($this->operation->save($data)) {
+				$return['errNo'] = 0;
+				$return['errMsg'] = '';
+			}else {
+				//注册失败
+				$return['errNo'] = 1002;
+				//$return['errMsg'] = $this->flash->output();
+			}
+			
+		}
 	}
 
 }
