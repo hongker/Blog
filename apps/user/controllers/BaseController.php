7da08294dc013372693a9621246f80f144d2b750
hongker <xiaok2013@live.com>
2015-08-30 20:36:57 +0800

    create user home framework

--- apps/user/controllers/BaseController.php
+++ apps/user/controllers/BaseController.php
@@ -38,7 +38,8 @@ class BaseController extends Controller
         //Prepend the application name to the title
         \Phalcon\Tag::prependTitle('Blog | ');
         $this->view->setTemplateAfter('common');
-        
+        $this->view->setVar('action',$this->action);
+        $this->view->setVar('controller',$this->controller);
     }
     
     /**
@@ -62,5 +63,53 @@ class BaseController extends Controller
      */
     public function json_return(Array $array) {
     	echo json_encode($array);exit;
+    }
+    
+    /**
+     * 获取GET数据
+     * @param string $param
+     * @param string $type
+     */
+    protected function getQuery($param,$type='string') {
+    	return $this->request->getQuery($param,$type);
+    }
+    
+    /**
+     * 检查用户是否登录
+     * @return boolean
+     */
+    protected function checkIsLogin() {
+    	if($this->session->has('user')) {
+    		return true;
+    	}
+    	return false;
+    }
+    
+    /**
+     * 获取POST数据
+     * @param string $param
+     * @param string $type
+     */
+    protected function getPost($param,$type='string') {
+    	return $this->request->getPost($param,$type);
+    }
+    
+    /**
+     * 获取分页类
+     * @param unknown $data
+     * @param number $currentPage
+     * @param number $limit
+     * @return unknown
+     */
+    public function getPaginate($data,$currentPage=1,$limit = 10) {
+    	$paginator = new \Phalcon\Paginator\Adapter\Model(
+    			array(
+    					"data"  => $data,
+    					"limit" => $limit,
+    					"page"  => $currentPage,
+    			));
+    	$pageinate = $paginator->getPaginate();
+    	 
+    	return $pageinate;
     }
 }
--- apps/user/controllers/HomeController.php
+++ apps/user/controllers/HomeController.php
@@ -3,21 +3,21 @@ namespace Blog\User\Controllers;
 use Blog\Operations\UserOperation;
 use Blog\Utils\Email;
 /**
- * 用户首页控制器
+ * 个人中心首页控制器
  * @author hongker
  * @version 1.0
  */
-class IndexController extends BaseController
+class HomeController extends BaseController
 {
 	public function initialize()
 	{
-		\Phalcon\Tag::setTitle('用户首页');
+		\Phalcon\Tag::setTitle('个人中心');
 		parent::initialize();
 		
 	}
 
 	public function indexAction(){
-		echo 'user';exit;		
+			
 	}
 	
 	
