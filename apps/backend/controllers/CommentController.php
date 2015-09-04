362876e2aafc0fd9f3ddd68dec208252dc87edf2
hongker <xiaok2013@live.com>
2015-09-04 16:02:20 +0800

    add comment lists in admin module

--- apps/backend/controllers/CommentController.php
+++ apps/backend/controllers/CommentController.php
@@ -1,28 +1,56 @@
 <?php
 namespace Blog\Backend\Controllers;
+use Blog\Operations\CommentOperation;
 /**
  * 评论管理控制器
  * @author hongker
  * @version 1.0
  */
-class IndexController extends BaseController
+class CommentController extends BaseController
 {
 	public function initialize()
 	{
 		\Phalcon\Tag::setTitle('评论管理');
 		parent::initialize();
+		$this->operation= new CommentOperation($this->di);
 	}
 
-	public function indexAction()
-	{
-		echo "<h1>admin!</h1>";
+	/**
+	 * 评论列表
+	 */
+	public function indexAction() {
+		$currentPage = $this->getQuery('page','int')?$this->getQuery('page','int'):1;
+		
+		$comments = $this->operation->findAll(array(
+				"conditions"=>"is_delete=0",
+				"order"=>"created_at desc",
+		));
+		
+		$page = $this->getPaginate($comments,$currentPage);
+		
+		$this->view->setVar('page',$page);
+		$this->view->setVar('currentType',0);
 	}
 	
 	/**
 	 * 删除评论
 	 */
 	public function deleteAction() {
-		
+		$return = array();
+		if($this->isPost()) {
+			$id = $this->getPost('id','int');
+				
+			if ($this->operation->delete( $id )) {
+				$return ['errNo'] = 0;
+			} else {
+				$return ['errNo'] = 1202;
+			}
+			
+		}else {
+			$return['errNo'] = 1002;
+		}
+		$return['errMsg'] = $this->getErrorMessage($return['errNo']);
+		$this->json_return($return);
 	}
 
 }
