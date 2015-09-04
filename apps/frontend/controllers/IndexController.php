949a25708a6f932bd4e98707a8c24633e49d821b
hongker <xiaok2013@live.com>
2015-09-04 20:09:35 +0800

    change article list style in index page

--- apps/frontend/controllers/IndexController.php
+++ apps/frontend/controllers/IndexController.php
@@ -2,6 +2,7 @@
 namespace Blog\Frontend\Controllers;
 use Blog\Operations\UserOperation;
 use Blog\Utils\Email;
+use Blog\Operations\ArticleOperation;
 /**
  * 首页控制器
  * @author hongker
@@ -17,7 +18,15 @@ class IndexController extends BaseController
 	}
 
 	public function indexAction(){
-			
+		$articleOperation = new ArticleOperation($this->di);
+		
+		$articles = $articleOperation->findAll(array(
+				"conditions"=>"is_delete=0",
+				"order"=>"created_at desc",
+				"limit"=>5,
+		));
+		
+		$this->view->setVar('articles',$articles);
 	}
 	
 	
