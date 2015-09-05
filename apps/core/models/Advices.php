ca748196669a39d18a75ef0f98a1bac2cd719ca1
hongker <xiaok2013@live.com>
2015-09-05 17:55:05 +0800

    add advice model validation

--- apps/core/models/Advices.php
+++ apps/core/models/Advices.php
@@ -15,4 +15,34 @@ class Advices extends BaseModel {
 		$this->skipAttributesOnCreate(array('status'));
 		
 	}
+	
+	/**
+	 * 验证字段是否合法
+	 * @return boolean
+	 */
+	public function validation() {
+		$validates = array();
+		$validates[] = new PresenceOf(array(
+				"field" => 'email',
+				"message" => 1501,
+		));
+		$validates[] = new PresenceOf(array(
+				"field" => 'name',
+				"message" => 1502,
+		));
+		$validates[] = new PresenceOf(array(
+				"field" => 'content',
+				"message" => 1503
+		));
+		$validates[] = new EmailValidator(array(
+				'field' => 'email',
+				'message' => 1505,
+		));
+		
+	
+		if($this->validateAll($validates)==false) {
+			return false;
+		}
+	
+	}
 }
